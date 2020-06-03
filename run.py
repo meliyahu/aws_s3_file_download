d14a9e2b9ac84c46f5ca67fcd58462e8b8b1deca
Mosheh EliYahu <mosheh.eliyahu@gmail.com>
2020-06-03 21:24:33 +0930

    Feat: Some mods

--- run.py
+++ run.py
@@ -1,9 +1,18 @@
-import boto3
+import boto3, botostubs
+from botocore.client import ClientError
 import logging
 import argparse
 import os
 import time
 from config import Config
+
+def does_bucket_exist(s3_bucket_name, s3_client):
+    try:
+        s3_client.head_bucket(Bucket=s3_bucket_name)
+        return None, True
+    except ClientError as err:
+        return err, False
+
 
 def get_latest_s3_file(s3_bucket_name, s3_client):
     
@@ -28,8 +37,11 @@ def run(s3_bucket_name: str):
                         datefmt='%d/%m/%Y %I:%M:%S %p')
 
     # Todo start the s3 download logic here.
-    s3_client = boto3.client('s3', aws_access_key_id=Config.AWS_ACCESS_KEY_ID , aws_secret_access_key=Config.AWS_SECRET_ACCESS_KEY)
+    s3_client = boto3.client('s3', aws_access_key_id=Config.AWS_ACCESS_KEY_ID , aws_secret_access_key=Config.AWS_SECRET_ACCESS_KEY) # type: botostubs.S3
+    
+    
     last_added_file = get_latest_s3_file(s3_bucket_name, s3_client)
+
     
     if last_added_file is None:
         raise
