e3d870b7bcb5142d281171522cb19ea9e6652d0b
Mosheh EliYahu <mosheh.eliyahu@gmail.com>
2020-06-11 09:31:02 +0930

    Feat: Added check for aws credentials

--- config.py
+++ config.py
@@ -2,12 +2,17 @@ import os
 import sys
 
 class Config:
-    
+  
+    # Not used  
     S3_BUCKET_NAME = os.environ.get('S3_BUCKET_NAME', 'tern_ausplots_uploads')
+    # Not used
     S3_OBJECT_NAME = os.environ.get('S3_OBJECT_NAME', 'ausplots_db_dump.dump')
 
-    AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID', 'my_aws_access_key_id') 
-    AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY', 'my_aws_secrete_access')
+    # Set env var for access_key_id and screte_access_key
+    AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID', '') 
+    AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY', '')
 
     ROOT_DIR = os.path.abspath(os.curdir)
+    
+    # Download folder
     DOWNLOADS_FOLDER = os.path.join(ROOT_DIR, 'downloads')  
--- s3_worker/s3_worker.py
+++ s3_worker/s3_worker.py
@@ -5,27 +5,36 @@ import argparse
 import os
 import time
 from config import Config
+import sys 
+
 
 class S3Worker():
     def __init__(self, bucket_name):
         self.bucket_name = bucket_name
+        self.check_creds()
         self.s3_client = boto3.client('s3', aws_access_key_id=Config.AWS_ACCESS_KEY_ID , aws_secret_access_key=Config.AWS_SECRET_ACCESS_KEY) # type: botostubs.S3
     
+    def check_creds(self):
+        if Config.AWS_ACCESS_KEY_ID is None or Config.AWS_SECRET_ACCESS_KEY is None:
+            sys.exit("Error: AWS_ACCESS_KEY_ID and AWS_SECRET_ACCESS_KEY missing! Please pass them as environment variables!")
+            
     def execute(self):
         
         # Todo check if bucket exists
         error, bucket_exist = self.does_bucket_exist()
         if bucket_exist == False:
-            logging.error(f'Bucket [{self.bucket_name}] does not exist! Error: {error}')
-            print(f'Bucket [{self.bucket_name}] does not exist! Error: {error}')
+            logging.error(f'Bucket [{self.bucket_name}] does not exist in AWS S3! {error}')
             print()
-            return
+            sys.exit(f"Bucket {self.bucket_name} does not exists in AWS S3! {error}")
         
         last_added_file = self.get_latest_s3_file()
         if last_added_file is None:
-            raise
-
+            logging.error(f'There are no files in bucket [{self.bucket_name}]!')
+            print()
+            sys.exit(f"There are no files in bucket [{self.bucket_name}]!")
+        
         download_file = os.path.join(Config.DOWNLOADS_FOLDER, last_added_file)
+           
         with open(download_file, 'wb') as f:
             self.s3_client.download_fileobj(self.bucket_name, last_added_file, f)
             logging.info(f'File {last_added_file} was downloaded from AWS S3 bucket successfully!')
